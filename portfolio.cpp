f71a9954fb25b594cd072f36c9911d0010775201
saralynhoward <saho3310@colorado.edu>
2025-02-16 06:32:52 -0700

    countDown/second thread done

--- portfolio.cpp
+++ portfolio.cpp
@@ -13,11 +13,22 @@ void countUp() {
         std::cout << "Thread 1 counting up: " << i << std::endl;
     }
 }
+void countDown() {
+    for (int i = 20; i >= 0; --i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cout << "Thread 2 counting down: " << i << std::endl;
+    }
+}
 int main() {
     std::thread thread1(countUp);
 
     // Wait for thread to finish
     thread1.join();
+
+    std::thread thread2(countDown);
+
+    thread2.join();
 
     return 0;
 }
